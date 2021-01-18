aa722941a93c4b51ead5af61db43ab762b47843c
Xavier <Xavier@SFNavarrosillo>
2021-01-18 13:00:01 +0100

    Segundo commit

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -13,8 +13,8 @@ public class Principal {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo2 java-git");
 	}
 
 }
