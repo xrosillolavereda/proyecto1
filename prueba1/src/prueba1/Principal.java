c895221eb1c267cd0fe14dab8793192ea57264dd
Xavier <Xavier@SFNavarrosillo>
2021-01-18 13:00:56 +0100

    Más commits

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -15,6 +15,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java-git");
 		System.out.println("Hola mundo2 java-git");
+		System.out.println("Hola mundo3 java-git");
 	}
 
 }
