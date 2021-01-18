937c404ce840819cbaffc6b9e1a082223620a6d7
Xavier <Xavier@SFNavarrosillo>
2021-01-18 12:52:07 +0100

    Primer commit

---
+++ prueba1/src/prueba1/Principal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package prueba1;
+
+/**
+ * @author Xavier
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
