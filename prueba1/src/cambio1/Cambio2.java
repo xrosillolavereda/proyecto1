73bcabce11cbe5223a53759b3c1660ff7a2a0865
Xavier <Xavier@SFNavarrosillo>
2021-01-19 13:15:38 +0100

    cagada

--- prueba1/src/cambio1/Cambio2.java
+++ prueba1/src/cambio1/Cambio2.java
@@ -3,7 +3,7 @@ package cambio1;
 public class Cambio2 {
 
 	public static void main(String[] args) {
-		System.out.println("Clase Cambio 2 en Proyecto 1");
+		System.out.println("Clase Cambio 2222 en Proyecto 1");
 
 	}
 
